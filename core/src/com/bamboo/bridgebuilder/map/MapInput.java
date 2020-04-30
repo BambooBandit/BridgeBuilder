4a0271be4728bcdfafe0137b29aca2f8097748c9
Siavash <bamboobandit.sr@gmail.com>
2020-04-30 02:36:11 -0500

    Fixed an issue where sometimes you would select a layer child despite clicking on a manipulator box

--- core/src/com/bamboo/bridgebuilder/map/MapInput.java
+++ core/src/com/bamboo/bridgebuilder/map/MapInput.java
@@ -68,12 +68,18 @@ public class MapInput implements InputProcessor
         Vector3 coords = Utils.unproject(map.camera, screenX, screenY);
         this.dragOrigin.set(coords.x, coords.y);
 
-        handleMapSpriteCreation(coords.x, coords.y, button);
-        handleSelect(button);
-        handleMapPointCreation(coords.x, coords.y, button);
-        handleMapPolygonVerticeCreation(coords.x, coords.y, button);
-        handleMapPolygonCreation(button);
-        handleManipulatorBoxTouchDown(coords.x, coords.y, button);
+        if(handleMapSpriteCreation(coords.x, coords.y, button))
+            return false;
+        if(handleManipulatorBoxTouchDown(coords.x, coords.y, button))
+            return false;
+        if(handleSelect(button))
+            return false;
+        if(handleMapPointCreation(coords.x, coords.y, button))
+            return false;
+        if(handleMapPolygonVerticeCreation(coords.x, coords.y, button))
+            return false;
+        if(handleMapPolygonCreation(button))
+            return false;
 
         return false;
     }
@@ -116,10 +122,10 @@ public class MapInput implements InputProcessor
         return false;
     }
 
-    private void handleManipulatorBoxTouchDown(float x, float y, int button)
+    private boolean handleManipulatorBoxTouchDown(float x, float y, int button)
     {
         if(button != Input.Buttons.LEFT)
-            return;
+            return false;
 
         for(int i = 0; i < map.selectedSprites.size; i++)
         {
@@ -128,15 +134,15 @@ public class MapInput implements InputProcessor
             {
                 this.moveMapSprites = new MoveMapSprites(this.map.selectedSprites);
                 this.map.pushCommand(this.moveMapSprites);
-                return;
+                return true;
             }
             else if(selectedSprite.rotationBox.contains(x, y))
             {
-                return;
+                return true;
             }
             else if(selectedSprite.scaleBox.contains(x, y))
             {
-                return;
+                return true;
             }
         }
 
@@ -147,26 +153,30 @@ public class MapInput implements InputProcessor
             {
                 this.moveMapObjects = new MoveMapObjects(this.map.selectedObjects);
                 this.map.pushCommand(this.moveMapObjects);
-                return;
+                return true;
             }
         }
+
+        return false;
     }
 
-    private void handleManipulatorBoxTouchUp()
+    private boolean handleManipulatorBoxTouchUp()
     {
         this.moveMapSprites = null;
         this.moveMapObjects = null;
+        return false;
     }
 
-    private void handleManipulatorBoxDrag(float x, float y)
+    private boolean handleManipulatorBoxDrag(float x, float y)
     {
         if(this.moveMapSprites != null)
             this.moveMapSprites.update(x, y);
         else if(this.moveMapObjects != null)
             this.moveMapObjects.update(x, y);
+        return false;
     }
 
-    private void handleHoveredLayerChildUpdate(float x, float y)
+    private boolean handleHoveredLayerChildUpdate(float x, float y)
     {
         if(map.selectedLayer != null && Utils.isFileToolThisType(editor, Tools.SELECT))
         {
@@ -176,106 +186,116 @@ public class MapInput implements InputProcessor
                 if (layerChild.isHoveredOver(x, y))
                 {
                     map.hoveredChild = layerChild;
-                    return;
+                    return false;
                 }
             }
         }
         map.hoveredChild = null;
+        return false;
     }
 
-    private void handleMapSpriteCreation(float x, float y, int button)
+    private boolean handleMapSpriteCreation(float x, float y, int button)
     {
         if(!Utils.isFileToolThisType(editor, Tools.BRUSH) || map.selectedLayer == null || !(map.selectedLayer instanceof SpriteLayer) || button != Input.Buttons.LEFT)
-            return;
+            return false;
 
         DrawMapSprite drawMapSprite = new DrawMapSprite(map, (SpriteLayer) map.selectedLayer, x, y);
         map.executeCommand(drawMapSprite);
+        return false;
     }
 
-    private void handleSelect(int button)
+    private boolean handleSelect(int button)
     {
         if(!Utils.isFileToolThisType(editor, Tools.SELECT) || map.selectedLayer == null || button != Input.Buttons.LEFT)
-            return;
+            return false;
         if(map.hoveredChild == null)
-            return;
+            return false;
 
         SelectLayerChild selectLayerChild = new SelectLayerChild(map, map.hoveredChild, Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT));
         map.executeCommand(selectLayerChild);
+        return false;
     }
 
-    private void handleMapPointCreation(float x, float y, int button)
+    private boolean handleMapPointCreation(float x, float y, int button)
     {
         if(!Utils.isFileToolThisType(editor, Tools.DRAWPOINT) || map.selectedLayer == null || !(map.selectedLayer instanceof ObjectLayer) || button != Input.Buttons.LEFT)
-            return;
+            return false;
 
         DrawMapPoint drawMapPoint = new DrawMapPoint(map, (ObjectLayer) map.selectedLayer, x, y);
         map.executeCommand(drawMapPoint);
+        return false;
     }
 
-    private void handleMapPolygonVerticeCreation(float x, float y, int button)
+    private boolean handleMapPolygonVerticeCreation(float x, float y, int button)
     {
         if(!Utils.isFileToolThisType(editor, Tools.DRAWOBJECT) || map.selectedLayer == null || !(map.selectedLayer instanceof ObjectLayer) || button != Input.Buttons.LEFT)
-            return;
+            return false;
 
         DrawMapPolygonVertice drawMapPolygonVertice = new DrawMapPolygonVertice(map, x, y, objectVerticePosition.x, objectVerticePosition.y);
         map.executeCommand(drawMapPolygonVertice);
+        return false;
     }
 
-    private void handleMapPolygonCreation(int button)
+    private boolean handleMapPolygonCreation(int button)
     {
         if(!Utils.isFileToolThisType(editor, Tools.DRAWOBJECT) || map.selectedLayer == null || !(map.selectedLayer instanceof ObjectLayer) || button != Input.Buttons.RIGHT)
         {
             if(button == Input.Buttons.RIGHT && map.input.mapPolygonVertices.size < 6)
                 clearMapPolygonVertices(button);
-            return;
+            return false;
         }
         if(map.input.mapPolygonVertices.size < 6)
         {
             clearMapPolygonVertices(button);
-            return;
+            return false;
         }
             DrawMapPolygon drawMapPolygon = new DrawMapPolygon(map, (ObjectLayer) map.selectedLayer, map.input.mapPolygonVertices, objectVerticePosition.x, objectVerticePosition.y);
         clearMapPolygonVertices(button);
         map.executeCommand(drawMapPolygon);
+        return false;
     }
 
-    private void clearMapPolygonVertices(int button)
+    private boolean clearMapPolygonVertices(int button)
     {
         if(button == Input.Buttons.RIGHT)
         {
             ClearMapPolygonVertices clearMapPolygonVertices = new ClearMapPolygonVertices(this.map, this.map.input.mapPolygonVertices);
             this.map.executeCommand(clearMapPolygonVertices);
         }
+        return false;
     }
 
-    private void handlePreviewSpritePositionUpdate(float x, float y)
+    private boolean handlePreviewSpritePositionUpdate(float x, float y)
     {
         SpriteTool spriteTool = map.getSpriteToolFromSelectedTools();
         if(spriteTool == null)
-            return;
+            return false;
         for(int i = 0; i < spriteTool.previewSprites.size; i ++)
         {
             Sprite previewSprite = spriteTool.previewSprites.get(i);
             previewSprite.setPosition(x - previewSprite.getWidth() / 2, y - previewSprite.getHeight() / 2);
         }
+        return false;
     }
 
-    private void handleCameraZoom(int amount)
+    private boolean handleCameraZoom(int amount)
     {
         if(Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
             amount *= 10;
         this.map.zoom += amount / 3f;
         if(this.map.zoom < .1f)
             this.map.zoom = .1f;
+        return false;
     }
 
-    private void handleCameraDrag()
+    private boolean handleCameraDrag()
     {
         if(!Utils.isFileToolThisType(editor, Tools.GRAB))
-            return;
+            return false;
         this.map.camera.position.x -= this.pos.x;
         this.map.camera.position.y -= this.pos.y;
         this.map.camera.update();
+        return false;
     }
 
     private int sign (int x) {
