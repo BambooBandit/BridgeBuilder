a3ac166818c4c7b9d8fe6d7c050fbefaebdf6a70
Siavash <bamboobandit.sr@gmail.com>
2020-06-02 16:18:19 -0500

    Fixed an issue where you could change a layers properties even when removed

--- core/src/com/bamboo/bridgebuilder/commands/RemoveLayer.java
+++ core/src/com/bamboo/bridgebuilder/commands/RemoveLayer.java
@@ -8,11 +8,13 @@ public class RemoveLayer implements Command
     private Map map;
     private Layer layer;
     private int layerIndex;
+    private boolean selected;
 
     public RemoveLayer(Map map, Layer layer)
     {
         this.map = map;
         this.layer = layer;
+        this.selected = layer.layerField.isSelected;
 
         for(int i = 0; i < this.map.layerMenu.layers.size; i ++)
         {
@@ -27,14 +29,24 @@ public class RemoveLayer implements Command
     @Override
     public void execute()
     {
+        if(this.selected)
+        {
+            this.layer.layerField.unselect();
+            this.map.selectedLayer = null;
+        }
         this.map.layerMenu.removeLayer(this.layer.layerField);
+        this.map.propertyMenu.rebuild();
     }
 
     @Override
     public void undo()
     {
         this.map.layerMenu.addLayer(this.layer, this.layerIndex);
-        if(this.layer.layerField.isSelected)
+        if(this.selected)
+        {
+            this.layer.layerField.select();
             this.map.selectedLayer = this.layer;
+        }
+        this.map.propertyMenu.rebuild();
     }
 }
