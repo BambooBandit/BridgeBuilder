e2afdff89c36ebdd9651144679f6f823a9363dde
Siavash Ranjbar <bamboobandit.sr@gmail.com>
2020-12-09 17:12:02 -0600

    window resize

--- desktop/src/com/bamboo/bridgebuilder/desktop/DesktopLauncher.java
+++ desktop/src/com/bamboo/bridgebuilder/desktop/DesktopLauncher.java
@@ -10,7 +10,7 @@ public class DesktopLauncher
 	{
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("BridgeBuilder");
-		config.setWindowedMode(1280, 720);
+		config.setWindowedMode(1920, 1080);
 		config.useVsync(true); // Setting to false disables vertical sync
 		config.setIdleFPS(0); // Setting to 0 disables background fps throttling
 		new Lwjgl3Application(new BridgeBuilder(), config);
