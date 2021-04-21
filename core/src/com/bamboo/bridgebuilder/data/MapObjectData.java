d6ed5d3c84a78c558717fc8bd1bfc56f5cefdbd8
Siavash Ranjbar <bamboobandit.sr@gmail.com>
2021-04-20 21:46:03 -0500

    Made every layer child have ID properties, not just MapSprites.

--- core/src/com/bamboo/bridgebuilder/data/MapObjectData.java
+++ core/src/com/bamboo/bridgebuilder/data/MapObjectData.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 public abstract class MapObjectData extends LayerChildData
 {
     public ArrayList<PropertyData> props;
+    public ArrayList<PropertyData> lProps; // locked properties
+    public long id;
 
     // Used for attached map objects only
     public float offsetX, offsetY;
@@ -16,6 +18,8 @@ public abstract class MapObjectData extends LayerChildData
     public MapObjectData(MapObject mapObject, float offsetX, float offsetY)
     {
         super(mapObject);
+
+        this.id = mapObject.id;
 
         this.props = new ArrayList<>();
         for(int i = 0; i < mapObject.properties.size; i ++)
@@ -29,6 +33,19 @@ public abstract class MapObjectData extends LayerChildData
                 this.props.add(new FieldFieldPropertyValuePropertyFieldData((FieldFieldPropertyValuePropertyField) property));
             else if(property instanceof LabelFieldPropertyValuePropertyField)
                 this.props.add(new LabelFieldPropertyValuePropertyFieldData((LabelFieldPropertyValuePropertyField) property));
+        }
+        this.lProps = new ArrayList<>();
+        for(int i = 0; i < mapObject.lockedProperties.size; i ++)
+        {
+            PropertyField property = mapObject.lockedProperties.get(i);
+            if(property instanceof ColorPropertyField)
+                this.lProps.add(new ColorPropertyFieldData((ColorPropertyField) property));
+            else if(property instanceof LightPropertyField)
+                this.lProps.add(new LightPropertyFieldData((LightPropertyField) property));
+            else if(property instanceof FieldFieldPropertyValuePropertyField)
+                this.lProps.add(new FieldFieldPropertyValuePropertyFieldData((FieldFieldPropertyValuePropertyField) property));
+            else if(property instanceof LabelFieldPropertyValuePropertyField)
+                this.lProps.add(new LabelFieldPropertyValuePropertyFieldData((LabelFieldPropertyValuePropertyField) property));
         }
 
         this.offsetX = offsetX;
